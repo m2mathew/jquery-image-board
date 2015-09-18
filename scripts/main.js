9361f5332e6323e2afb72fa4bff3456637b4b54e
Mike Mathew <m2mathew@me.com>
2015-09-18 14:44:32 -0500

    All regular mode functions are working. Still getting form styling and media queries finalized.

--- scripts/main.js
+++ scripts/main.js
@@ -8,23 +8,75 @@ $(document).ready(function(){
     var $form = $('form');
     var $cancelButton = $('#cancel-button');
     var $gallery = $('#gallery');
-    var url = 'http://tiyfe.herokuapp.com/collections/mike_m_jquery_image_board';
+    var $deleteAllButton = $('#delete-all-button');
+    var url = 'http://tiyfe.herokuapp.com/collections/mike_m_images/';
 
     // Initialize material box for picture viewing
     $('.materialboxed').materialbox();
 
+    // $.click(function() {
+    //     $form.toggle();
+    // })
+
+    // function to show images in gallery
+    var displayGallery = function() {
+        $imageUrl.val('');
+        $imageCaption.val('');
+        $gallery.html('');
+
+        $.get(
+            url,
+            function(response){
+                response.forEach(function(response, index) {
+                    var newString = '<div id="photo-box"><img class="materialboxed" data-caption="' + response.caption + '" src="' + response.url + '"><p>' + response.caption + '</p></div>';
+                    $gallery.append(newString);
+            }),
+            'json'
+        });
+    };
+
+    // call function to start page with current gallery images
+    displayGallery();
+
+    // actions that happen when form is submitted
     $form.submit(function(e) {
         e.preventDefault();
 
-        $imageUrl.val('');
-        $imageCaption.val('');
-        $imageCaption.trigger('autoresize');
+        var newUrl = $imageUrl.val();
+        var newCaption = $imageCaption.val();
 
-
-
+        $.post(
+                url,
+                {
+                    url: newUrl,
+                    caption: newCaption
+                },
+                displayGallery,
+                'json'
+            );
     });
 
 
+    // function to Delete All pictures from the server
+    function onDeleteAll() {
+        $.get(
+            url,
+            function (response) {
+                response.forEach(function(record) {
+                    var newUrl = url + record._id;
+                    $.ajax({
+                        url: newUrl,
+                        method: 'DELETE'
+                    });
+                });
+            },
+            'json'
+        );
+        $imageUrl.val('');
+        $imageCaption.val('');
+        $gallery.html('');
+    }
 
+    $deleteAllButton.click(onDeleteAll);
 
 });
