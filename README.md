196e74178e19948847d27fa4b04bf6dc0f7544e7
Mike Mathew <m2mathew@me.com>
2015-11-21 23:26:13 -0600

    Updated readme

--- README.md
+++ README.md
@@ -1,4 +1,64 @@
-# tiyfe-template
-test
-dds
-sdfsdf
+# Backbone Image Board
+
+Assignment from The Iron Yard-Austin
+
+Week 4 - day 16
+
+## Description
+
+Create an image board that saves submissions to tiny pizza server.
+
+
+## Objectives
+
+### Learning Objectives
+
+After completing this assignment, you should…
+
+* Understand how to use AJAX to save data to a server
+
+
+### Performance Objectives
+
+After completing this assignment, you be able to effectively use
+
+* $.get / $.post
+* Event listeners
+* Data Validation
+
+## Details
+
+### Deliverables
+
+* A repo containing at least:
+  * `index.html`
+  * `scripts/main.js`
+
+### Requirements
+
+* No ESLint warnings or errors
+
+
+## Normal Mode
+
+Using the tools you've learned in class, create a responsive image board that allows you to add images and captions via a url. The images that are added to the image board should be saved to tiny pizza server, so that when you reload the page, they are not lost. The form to add an image should properly validate the image URL by at minimum checking for a http:// or https:// prefix and require a non-empty description. Below are screenshots of what your app should look like.
+
+### Mobile
+
+![Mobile](https://github.com/TIY-Austin-Front-End-Engineering/jquery-image-board/blob/master/mobile.png)
+
+### Desktop
+
+![Desktop](https://github.com/TIY-Austin-Front-End-Engineering/jquery-image-board/blob/master/desktop.png)
+
+### Add Image Form
+
+![Add](https://github.com/TIY-Austin-Front-End-Engineering/jquery-image-board/blob/master/add.png)
+
+## Hard Mode
+
+Allow users to register for an account using a username and password. Force them to log in to their account (validating that they enter the correct username and password) before they are able to post images to their image board. Each user should only see images that they posted.
+
+## Notes
+
+Check out [ionicons](http://ionicons.com/) for the icons used.
--- scripts/main.js
+++ scripts/main.js
@@ -2,130 +2,129 @@
 
 $(document).ready(function(){
 
-    // target elements needed
-    var $imageUrl = $('#image-url');
-    var $imageCaption = $('.image-caption');
-    var $errorUrl = $('#error-url');
-    var $errorCaption = $('#error-caption');
-    var $toggleButton = $('.toggle-button');
-    var $toggleIcon = $('#toggle-icon');
-    var $bigFormBox = $('.form-big-box');
-    var $form = $('form');
-    var $cancelButton = $('#cancel-button');
-    var $galleryBox = $('.gallery-box');
-    var $gallery = $('#gallery');
-    var $deleteAllButton = $('#delete-all-button');
-    var url = 'http://tiyfe.herokuapp.com/collections/mike_m_image_board/';
+	// target elements needed
+	var $imageUrl = $('#image-url');
+	var $imageCaption = $('.image-caption');
+	var $errorUrl = $('#error-url');
+	var $errorCaption = $('#error-caption');
+	var $toggleButton = $('.toggle-button');
+	var $toggleIcon = $('#toggle-icon');
+	var $bigFormBox = $('.form-big-box');
+	var $form = $('form');
+	var $cancelButton = $('#cancel-button');
+	var $galleryBox = $('.gallery-box');
+	var $gallery = $('#gallery');
+	var $deleteAllButton = $('#delete-all-button');
+	var url = 'http://tiyfe.herokuapp.com/collections/mike_m_image_board/';
 
-    // Initialize material box for picture viewing
-    $('.materialboxed').materialbox();
+	// Initialize material box for picture viewing
+	$('.materialboxed').materialbox();
 
-    // toggles the form element to show or hide
-    $toggleButton.click(function() {
-        if($toggleIcon.html() === 'expand_more') {
-            $toggleIcon.html('expand_less');
-            $bigFormBox.show(200);
-        } else if($toggleIcon.html() === 'expand_less') {
-            $toggleIcon.html('expand_more');
-            $bigFormBox.hide(200);
-        }
+	// toggles the form element to show or hide
+	$toggleButton.click(function() {
+		if($toggleIcon.html() === 'expand_more') {
+			$toggleIcon.html('expand_less');
+			$bigFormBox.show(200);
+		} else if($toggleIcon.html() === 'expand_less') {
+			$toggleIcon.html('expand_more');
+			$bigFormBox.hide(200);
+		}
+	});
 
-    });
+	// cancel button clears input fields
+	$cancelButton.click(function() {
+		$imageUrl.val('');
+		$imageCaption.val('');
+	});
 
-    // cancel button clears input fields
-    $cancelButton.click(function() {
-        $imageUrl.val('');
-        $imageCaption.val('');
-    });
+	// function to show images in gallery
+	var displayGallery = function() {
+		$imageUrl.val('');
+		$imageCaption.val('');
+		$gallery.html('');
+		$gallery.show();
+		$deleteAllButton.show();
 
-    // function to show images in gallery
-    var displayGallery = function() {
-        $imageUrl.val('');
-        $imageCaption.val('');
-        $gallery.html('');
-        $gallery.show();
-        $deleteAllButton.show();
+		$.get(
+			url,
+			function(response){
+				response.forEach(function(response, index) {
+					var newString = '<div id="photo-box"><img class="materialboxed" data-caption="' + response.caption + '" src="' + response.url + '"><p>' + response.caption + '</p></div>';
+					$gallery.append(newString);
+			}),
+			'json'
+		});
+	};
 
-        $.get(
-            url,
-            function(response){
-                response.forEach(function(response, index) {
-                    var newString = '<div id="photo-box"><img class="materialboxed" data-caption="' + response.caption + '" src="' + response.url + '"><p>' + response.caption + '</p></div>';
-                    $gallery.append(newString);
-            }),
-            'json'
-        });
-    };
+	// call function to start page with current gallery images
+	displayGallery();
 
-    // call function to start page with current gallery images
-    displayGallery();
+	// actions that happen when form is submitted
+	$form.submit(function(e) {
+		e.preventDefault();
+		var newUrl = $imageUrl.val();
+		var newCaption = $imageCaption.val();
+		var validUrl = true;
+		var validCaption = true;
 
-    // actions that happen when form is submitted
-    $form.submit(function(e) {
-        e.preventDefault();
-        var newUrl = $imageUrl.val();
-        var newCaption = $imageCaption.val();
-        var validUrl = true;
-        var validCaption = true;
+		// validate URL
+		if ( ((newUrl.indexOf('http://') !== -1) || (newUrl.indexOf('https://') !== -1)) && ((newUrl.indexOf('.jpg') !== -1) || (newUrl.indexOf('.jpeg') !== -1) || (newUrl.indexOf('.gif') !== -1) || (newUrl.indexOf('.bmp') !== -1) || (newUrl.indexOf('.webp') !== -1) || (newUrl.indexOf('.png') !== -1)) ) {
+			$errorUrl.html('');
+			$imageUrl.css( {borderLeft: 'none'});
+			validUrl = true;
+		}
+		else {
+			$errorUrl.html('<span>Please input a valid URL in a picture format.</span>');
+			$imageUrl.css( {borderLeft: '4px solid red'});
+			validUrl = false;
+		}
 
-        // validate URL
-        if ( ((newUrl.indexOf('http://') !== -1) || (newUrl.indexOf('https://') !== -1)) && ((newUrl.indexOf('.jpg') !== -1) || (newUrl.indexOf('.jpeg') !== -1) || (newUrl.indexOf('.gif') !== -1) || (newUrl.indexOf('.bmp') !== -1) || (newUrl.indexOf('.webp') !== -1) || (newUrl.indexOf('.png') !== -1)) ) {
-            $errorUrl.html('');
-            $imageUrl.css( {borderLeft: 'none'});
-            validUrl = true;
-        }
-        else {
-            $errorUrl.html('<span>Please input a valid URL in a picture format.</span>');
-            $imageUrl.css( {borderLeft: '4px solid red'});
-            validUrl = false;
-        }
+		// validate caption
+		if (newCaption !== '' || newCaption !== ' ') {
+			$errorCaption.html('');
+			$imageCaption.css( {borderLeft: '1px solid black'});
+			validCaption = true;
+		}
+		else {
+			$errorCaption.html('<span>Please submit a caption.</span>');
+			$imageCaption.css( {borderLeft: '4px solid red'});
+			validCaption = false;
+		}
 
-        // validate caption
-        if (newCaption !== '' || newCaption !== ' ') {
-            $errorCaption.html('');
-            $imageCaption.css( {borderLeft: '1px solid black'});
-            validCaption = true;
-        }
-        else {
-            $errorCaption.html('<span>Please submit a caption.</span>');
-            $imageCaption.css( {borderLeft: '4px solid red'});
-            validCaption = false;
-        }
+		if (validUrl === true && validCaption === true) {
+			$.post(
+				url,
+				{
+					url: newUrl,
+					caption: newCaption
+				},
+				displayGallery,
+				'json'
+			);
+		}
+	});
 
-        if (validUrl === true && validCaption === true) {
-            $.post(
-                    url,
-                    {
-                        url: newUrl,
-                        caption: newCaption
-                    },
-                    displayGallery,
-                    'json'
-                );
-        }
-    });
+	// function to Delete All pictures from the server
+	function onDeleteAll() {
+		$.get(
+			url,
+			function (response) {
+				response.forEach(function(record) {
+					var newUrl = url + record._id;
+					$.ajax({
+						url: newUrl,
+						method: 'DELETE'
+					});
+				});
+			},
+			'json'
+		);
+		$imageUrl.val('');
+		$imageCaption.val('');
+		$gallery.html('');
+		$gallery.hide();
+	}
 
-    // function to Delete All pictures from the server
-    function onDeleteAll() {
-        $.get(
-            url,
-            function (response) {
-                response.forEach(function(record) {
-                    var newUrl = url + record._id;
-                    $.ajax({
-                        url: newUrl,
-                        method: 'DELETE'
-                    });
-                });
-            },
-            'json'
-        );
-        $imageUrl.val('');
-        $imageCaption.val('');
-        $gallery.html('');
-        $gallery.hide();
-    }
-
-    $deleteAllButton.click(onDeleteAll);
+	$deleteAllButton.click(onDeleteAll);
 
 });
